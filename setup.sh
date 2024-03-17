7dff794182130b9107b5e47d053aae6682d130a8
Phubordin Poolnai <mirailisclm@gmail.com>
2024-03-17 23:28:37 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -49,7 +49,7 @@ brew_install "rectangle"
 
 if brew_install "neovim"; then
     echo "\nInstalling Vim-Plug"
-    sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \ https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim';
+    sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim';
     else
         echo "\nVim-Plug is already installed"
 fi
